e1f39e8e657be8edc37bf2722483202593cfb6b2
Baptiste <bburles@gmail.com>
2013-04-10 16:09:27 +0200

    UART.

--- babcode/platform/unix/uart_unix.c
+++ babcode/platform/unix/uart_unix.c
@@ -1,9 +1,160 @@
+// Copyright (c) 2013, Baptiste Burles
 //
-//  uart_unix.c
-//  babcode-macosx
+// All rights reserved.
 //
-//  Created by Baptiste on 10/04/13.
-//  Copyright (c) 2013 Baptiste. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
 //
+// * Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+// * Neither the name of the University of California, Berkeley nor the
+//   names of its contributors may be used to endorse or promote products
+//   derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY Baptiste Burles AND CONTRIBUTORS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL Baptiste Burles AND CONTRIBUTORS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include <stdio.h>
+#include "../../include/babcode.h"
+
+#include <sys/ioctl.h>
+
+
+extern int get_fd_uart(void);
+
+UInt32 UARTGetStatus()
+{
+    int status;
+    
+    ioctl(get_fd_uart(), TIOCMGET, &status);
+    
+    return status;
+}
+
+void UARTClearDTR()
+{
+    UInt32 status = 0;
+    
+    ioctl(get_fd_uart(), TIOCMGET, &status);
+    
+    status |= TIOCM_DTR;
+    
+    ioctl(get_fd_uart(), TIOCMSET, &status);
+}
+
+void UARTSetDTR()
+{
+    UInt32 status = 0 ;
+    
+    ioctl(get_fd_uart(), TIOCMGET, &status);
+    
+    status &= ~TIOCM_DTR;
+    
+    ioctl(get_fd_uart(), TIOCMSET, &status);
+}
+
+void UARTClearRTS()
+{
+    UInt32 status = 0;
+    
+    ioctl(get_fd_uart(), TIOCMGET, &status);
+    
+    status |= TIOCM_RTS;
+    
+    ioctl(get_fd_uart(), TIOCMSET, &status);
+}
+
+void UARTSetRTS()
+{
+    UInt32 status = 0 ;
+    
+    ioctl(get_fd_uart(), TIOCMGET, &status);
+    
+    status &= ~TIOCM_RTS;
+    
+    ioctl(get_fd_uart(), TIOCMSET, &status);
+}
+
+
+
+UInt8 UARTisDSRset()
+{
+    UInt32 status = 0;
+    
+    ioctl(get_fd_uart(), TIOCMGET, &status);
+    
+    if(status & TIOCM_DSR)
+        return 0;
+    else
+        return 1;
+}
+
+UInt8 UARTisDTRset()
+{
+    UInt32 status = 0;
+    
+    ioctl(get_fd_uart(), TIOCMGET, &status);
+    
+    if(status & TIOCM_DTR)
+        return 0;
+    else
+        return 1;
+}
+
+UInt8 UARTisRTSset()
+{
+    UInt32 status = 0;
+    
+    ioctl(get_fd_uart(), TIOCMGET, &status);
+    
+    if(status & TIOCM_RTS)
+        return 0;
+    else
+        return 1;
+}
+
+UInt8 UARTisCTSset()
+{
+    UInt32 status = 0;
+    
+    ioctl(get_fd_uart(), TIOCMGET, &status);
+    
+    if(status & TIOCM_CTS)
+        return 0;
+    else
+        return 1;
+}
+
+UInt8 UARTisDCDset()
+{
+    UInt32 status = 0;
+    
+    ioctl(get_fd_uart(), TIOCMGET, &status);
+    
+    if(status & TIOCM_CAR)
+        return 0;
+    else
+        return 1;
+}
+
+UInt8 UARTisRNGset()
+{
+    UInt32 status = 0;
+    
+    ioctl(get_fd_uart(), TIOCMGET, &status);
+    
+    if(status & TIOCM_RNG)
+        return 0;
+    else
+        return 1;
+}
