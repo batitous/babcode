5f910f7d5132ff76a7a56950a24f1d4c4dd7997a
Sylvain Fay-Chatelard <sylvain.faychatelard@gmail.com>
2013-07-11 16:38:30 +0200

    Add bytestream object.

--- babcode/include/bytestream.h
+++ babcode/include/bytestream.h
@@ -25,40 +25,43 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef BABCODE_H
-#define BABCODE_H
+#ifndef BABCODE_BYTESTREAM_H
+#define BABCODE_BYTESTREAM_H
 
-// This file must be included in your main code.
-
-#define PLATFORM_WINDOWS  1
-#define PLATFORM_MAC      2
-#define PLATFORM_UNIX     3
-
-#if defined(_WIN32)
-#   define PLATFORM PLATFORM_WINDOWS
-#elif defined(__APPLE__)
-#   define PLATFORM PLATFORM_MAC
-#else
-#   define PLATFORM PLATFORM_UNIX
+#ifdef __cplusplus
+extern "C" {
 #endif
 
+typedef struct _byte_stream_
+{
+    unsigned char * buffer;
+    unsigned char * current;
+} ByteStream;
 
-#include "types.h"
-#include "log.h"
-#include "utils.h"
-#include "hashtable.h"
-#include "conversion.h"
-#include "random.h"
-#include "file.h"
-#include "wait.h"
-#include "uart.h"
-#include "thread.h"
-#include "mutex.h"
-#include "conditionvar.h"
-#include "networkstack.h"
-#include "str.h"
-#include "list.h"
-#include "timer.h"
-#include "bytestream.h"
+extern void set4ToBuffer(unsigned char *buffer, unsigned int integer);
+
+
+extern ByteStream * newByteStream(unsigned char * input);
+
+extern void resetByteStream(ByteStream * stream);
+
+extern unsigned int getByteStreamSize(ByteStream * stream);
+
+extern void write1ToByteStream(ByteStream * stream, unsigned char data);
+
+extern void write2ToByteStream(ByteStream * stream, unsigned short data);
+
+extern void write4ToByteStream(ByteStream * stream, unsigned int data);
+
+extern void writeBufferToByteStream(ByteStream * stream, unsigned char * input, unsigned int size);
+
+
+extern unsigned char read1FromByteStream(ByteStream * stream);
+extern unsigned int read4FromByteStream(ByteStream * stream);
+extern void readBufferFromByteStream(ByteStream * stream, unsigned char * output, unsigned int size);
+    
+#ifdef __cplusplus
+}
+#endif
 
 #endif
---
+++ babcode/source/bytestream.c
@@ -0,0 +1,126 @@
+// Copyright (c) 2013, Sylvain Fay-Chatelard
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+// * Neither the name of the University of California, Berkeley nor the
+//   names of its contributors may be used to endorse or promote products
+//   derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY Sylvain Fay-Chatelard AND CONTRIBUTORS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL Baptiste Burles AND CONTRIBUTORS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "../include/bytestream.h"
+
+unsigned int get4FromBuffer(unsigned char *buffer)
+{
+	unsigned int integer = ((buffer)[0]<<24)| ((buffer)[1]<<16)|((buffer)[2]<<8)|((buffer)[3]);
+    
+	return integer;
+}
+
+void set4ToBuffer(unsigned char *buffer, unsigned int integer)
+{
+	buffer[0] = (integer>>24);
+	buffer[1] = (integer>>16);
+	buffer[2] = (integer>>8);
+	buffer[3] = (integer);
+}
+
+unsigned short get2FromBuffer(unsigned char *buffer)
+{
+	unsigned short integer = ((buffer)[0]<<8)|((buffer)[1]);
+    
+	return integer;
+}
+
+void set2ToBuffer(unsigned char *buffer, unsigned short integer)
+{
+	buffer[0] = (integer>>8);
+	buffer[1] = (unsigned char)(integer);
+}
+
+
+ByteStream * newByteStream(unsigned char * input)
+{
+    ByteStream * stream = (ByteStream *)malloc(sizeof(ByteStream));
+    
+    stream->buffer = input;
+    stream->current = stream->buffer;
+    
+    return stream;
+}
+
+void resetByteStream(ByteStream * stream)
+{
+    stream->current = stream->buffer;
+}
+
+unsigned int getByteStreamSize(ByteStream * stream)
+{
+    return (unsigned int)(stream->current - stream->buffer);
+}
+
+void write1ToByteStream(ByteStream * stream, unsigned char data)
+{
+    *stream->current = data;
+    stream->current++;
+}
+
+void write2ToByteStream(ByteStream * stream, unsigned short data)
+{
+    set2ToBuffer(stream->current, data);
+    stream->current += 2;
+}
+
+void write4ToByteStream(ByteStream * stream, unsigned int data)
+{
+    set4ToBuffer(stream->current, data);
+    stream->current += 4;
+}
+
+void writeBufferToByteStream(ByteStream * stream, unsigned char * input, unsigned int size)
+{
+    memcpy(stream->current, input,size);
+    stream->current += size;
+}
+
+unsigned char read1FromByteStream(ByteStream * stream)
+{
+    unsigned char data = *stream->current;
+    stream->current++;
+    
+    return data;
+}
+
+unsigned int read4FromByteStream(ByteStream * stream)
+{
+    int integer = (stream->current[0]<<24)| (stream->current[1]<<16)|(stream->current[2]<<8)|(stream->current[3]);
+	    
+	stream->current +=4;
+    return integer;
+}
+
+void readBufferFromByteStream(ByteStream * stream, unsigned char * output, unsigned int size)
+{
+    memcpy(output, stream->current,size);
+    stream->current += size;
+}
