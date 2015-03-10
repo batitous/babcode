8490f460af401e3516e51e6de17f56ad84f1fc72
batitous <bburles@gmail.com>
2015-03-10 18:21:36 +0100

    Clean vector include file

---
+++ babextended/include/vector.h
@@ -0,0 +1,70 @@
+// Copyright (c) 2015, Baptiste Burles
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+// * Neither the name of Baptiste Burles nor the
+//   names of its contributors may be used to endorse or promote products
+//   derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY Baptiste Burles AND CONTRIBUTORS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL Baptiste Burles AND CONTRIBUTORS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef babextended_vector_h
+#define babextended_vector_h
+
+
+#include <string.h>
+
+
+/** Minimal vector object
+ *
+ */
+template<typename Data>
+class Vector
+{
+public:
+    Vector();
+    Vector(Vector const &other);
+    
+    ~Vector();
+    
+    Vector &operator=(Vector const &other);
+    
+    void push(Data const &x);
+    
+    size_t size() const;
+    
+    Data const &operator[](size_t idx) const;
+    
+    Data &operator[](size_t idx);
+    
+private:
+    size_t  mSize;
+    size_t  mCapacity;
+    Data *  mData;
+    
+    void resize();
+
+};
+
+
+#include "vectorimpl.h"
+
+
+#endif
